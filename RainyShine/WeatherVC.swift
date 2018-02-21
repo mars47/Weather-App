4e0ad4e86d58635a5dca29ec304352a7a5a6a164
Omar <omar.amoako@yahoo.co.uk>
2018-02-21 19:35:04 +0000

    Designed the interface in Main. Storyboard Linked up IBOutlets to WeatherVC

---
+++ RainyShine/WeatherVC.swift
@@ -0,0 +1,48 @@
+//
+//  WeatherVC.swift
+//  RainyShine
+//
+//  Created by Omar  on 30/01/2018.
+//  Copyright © 2018 Omar. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var currentTempLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var currentWeatherImage: UIImageView!
+    @IBOutlet weak var currentWeatherTypeLabel: UILabel!
+    
+    var currentWeather = CurrentWeather()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print(CURRENT_WETHER_URL)
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        currentWeather.downloadWeatherDetails{
+            
+        }
+        
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell", for: indexPath)
+        
+        return cell
+    }
+}
+
