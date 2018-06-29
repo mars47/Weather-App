4cd01006bc94d0c8a71cf81915062bf78defbb15
Omar <omar.amoako@yahoo.co.uk>
2018-06-29 22:13:21 +0100

    Configured Model, Completed Network Request, Updated ViewController

--- RainyShine/Constants.swift
+++ RainyShine/Constants.swift
@@ -11,11 +11,16 @@ let BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
 let LATITUDE = "lat="
 let LONGITUDE = "&lon="
 let APP_ID = "&appid="
-let API_KEY = "073a5f570651811ba827086269ba251c"
+let API_KEY = "42a1771a0b787bf12e734ada0cfc80cb"
 
-let CURRENT_WETHER_URL = "\(BASE_URL)\(LATITUDE)-36\(LONGITUDE)123\(APP_ID)\(API_KEY)"
+let CURRENT_WETHER_URL = "\(BASE_URL)\(LATITUDE)51.5685\(LONGITUDE)0.0100\(APP_ID)\(API_KEY)"
+let FORECAST_URL = "http://api.openweathermap.org/data/2.5/forecast/daily?lat=51.5685&lon=0.0100&cnt=10&mode=json&appid=42a1771a0b787bf12e734ada0cfc80cb"
 
 typealias DownloadComplete = () -> ()
 
 // http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=073a5f570651811ba827086269ba251c
 //http://api.openweathermap.org/data/2.5/weather?lat=-36&lon=123&appid=073a5f570651811ba827086269ba251c
+
+//http://api.openweathermap.org/data/2.5/weather?lat=-36&lon=123&appid=42a1771a0b787bf12e734ada0cfc80cb
+//http://api.openweathermap.org/data/2.5/forecast/daily?lat=51.5685&lon=0.0100&cnt=10&mode=json&appid=42a1771a0b787bf12e734ada0cfc80cb
+//"073a5f570651811ba827086269ba251c"
